97f60cd39157eae670c0daaa7edc9176211f058d
samshimanta <nirobshimanta@gmail.com>
2025-07-29 13:04:02 +0600

    test git

--- src/app/@pages/landing/components/topbar-widget.ts
+++ src/app/@pages/landing/components/topbar-widget.ts
@@ -56,6 +56,11 @@ import { ButtonModule } from 'primeng/button';
                         <span>Pricing</span>
                     </a>
                 </li>
+                 <li>
+                        <a (click)="router.navigate(['/landing'], { fragment: 'pricing' })" pRipple class="px-0 py-4 text-surface-900 dark:text-surface-0 font-medium text-xl">
+                        <span>TEST GIT</span>
+                    </a>
+                </li>
             </ul>
             <div class="flex border-t lg:border-t-0 border-surface py-4 lg:py-0 mt-4 lg:mt-0 gap-2">
                 <button pButton pRipple label="Login" routerLink="/auth/login" [rounded]="true" [text]="true"></button>
