b9fd486bc01561b15fe3e0edd061f9aa40237214
samshimanta <nirobshimanta@gmail.com>
2025-07-29 13:16:53 +0600

    git new account push

--- src/app/@pages/landing/components/feature-widget/feature-widget.ts
+++ src/app/@pages/landing/components/feature-widget/feature-widget.ts
@@ -16,6 +16,11 @@ import { CommonModule } from '@angular/common';
                 <span class="text-muted-color text-2xl">Dummy Text Lorem Ipsum</span>
             </div>
 
+             <div class="col-span-12 text-center mt-20 mb-6">
+                <div class="text-surface-900 dark:text-surface-0 font-normal mb-2 text-4xl">Marvelous Features</div>
+                <span class="text-muted-color text-2xl">Dummy Text Lorem Ipsum testing Git</span>
+            </div>
+
             <div class="col-span-12 md:col-span-12 lg:col-span-4 p-0 lg:pr-8 lg:pb-8 mt-6 lg:mt-0">
                 <div style="height: 160px; padding: 2px; border-radius: 10px; background: linear-gradient(90deg, rgba(253, 228, 165, 0.2), rgba(187, 199, 205, 0.2)), linear-gradient(180deg, rgba(253, 228, 165, 0.2), rgba(187, 199, 205, 0.2))">
                     <div class="p-4 bg-surface-100 dark:bg-surface-900 h-full" style="border-radius: 8px">
